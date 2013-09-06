0f577175ad34324b21d9d0d5df4744f3a07caff1
Paul Cowan <paul@monospacesoftware.com>
2013-09-06 19:15:23 -0400

    indicator LED working

--- lib/client.py
+++ lib/client.py
@@ -12,6 +12,7 @@ from datetime import datetime
 from ConfigParser import SafeConfigParser
 import M2Crypto
 from M2Crypto import EVP
+from subprocess import call
 from kegnet import w1therm
 
 TIME_CHECK_TS = 1262304000
@@ -28,6 +29,8 @@ nextRetry = 0
 retryCount = 0
 
 syslog.openlog("kegnet-client", logoption=syslog.LOG_PID|syslog.LOG_CONS, facility=syslog.LOG_USER)
+
+call(["gpio", "mode", "2", "output"])
 
 def log(level, message, dumpStack=True):
   syslog.syslog(level, message)
@@ -98,6 +101,12 @@ def getTemp():
       log(syslog.LOG_ERR, "failed to read temp: {0}".format(e), False)
       return -1
   return lastTemp
+
+def setLED(status):
+  code = "off"
+  if (status):
+    code = "on"
+  call(["gpio", "write", "2", code])
   
 def failPour(path):
   newPath = path + ".fail"
@@ -287,11 +296,10 @@ def ping():
     return True
   elif 500 <= response.status_code < 600:
     log(syslog.LOG_INFO, "KegNet refused ping '{0}': {1}".format(signData, response))
-    return False
+    return True
   else:
     log(syslog.LOG_INFO, "KegNet failed ping '{0}': {1}".format(signData, response))
     return False
-
 
 if time.time() < TIME_CHECK_TS:
   log(syslog.LOG_WARNING, "waiting for the system to synch the local clock...")
@@ -304,15 +312,18 @@ log(syslog.LOG_NOTICE, "starting with baseUrl '{0}' and uuid '{1}'".format(servi
 
 try:
   while True:
-    ping()
+    setLED(ping())
     while notifier.check_events():
       notifier.read_events()
       notifier.process_events()
     processRetries()
 except KeyboardInterrupt as e:
   log(syslog.LOG_NOTICE, "shutting down")
+  setLED(False)
 except Exception as e:
   log(syslog.LOG_ERR, "caught unexpected exception during main loop: {0}".format(e))
+  setLED(False)
   raise
 
+setLED(False)
 notifier.stop();
